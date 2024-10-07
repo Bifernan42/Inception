e8b42f7a4de32bf94a8d6668c864e99ffaef8beb
root <root@Inception.bifernan.42.fr>
2024-10-07 16:30:34 +0200

    base

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+service mysql start
+
+sleep 5
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_DATABASE}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+exec mysql_safe
